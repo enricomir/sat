e5eb60058829f9bf543b95b7a333903cdd729ac0
enricomir <enrico.s.miranda@gmail.com>
2015-09-14 23:20:22 -0300

    Include Branch and Bound

--- src/main.cc
+++ src/main.cc
@@ -8,17 +8,10 @@ int main(int argc, char* argv[]) {
 		std::cerr << "Error! Usage: ./sat.app <filename>" << std::endl;
 		return 1;
 	}
+
 	SatProblem p(argv[1]);
 	p.printClauses();
-/*
-	std::cout << "c Eval: " << p.eval() << "\n";
 
-	for (int i = 0; i < p.allocated.size(); ++i) {
-		p.allocated[i] = true;
-	}
-
-	std::cout << "c Eval: " << p.eval() << "\n";
-*/
 	BnB c(p);
 
 	c.solve(std::chrono::seconds(5));
